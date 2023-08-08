88e924e9d438deca77e7c859cff36456b3f34afe
liwenlong <vinlon1009@gmail.com>
2023-08-08 20:07:52 +0800

    mod: 调整目录结构

--- blessing/batch_blessing.py
+++ blessing/batch_blessing.py
@@ -3,13 +3,14 @@ import json
 import os
 from web3 import Web3
 
-# 合约地址: https://app.roninchain.com/address/ronin:9d3936dbd9a794ee31ef9f13814233d435bd806c
-blessing_contract_address='0x9d3936dbd9a794ee31ef9f13814233d435bd806c'
-ronin_rpc = 'https://api.roninchain.com/rpc'
-abi_file_path = os.path.abspath('abis/blessing_abi.json')
 
+### ============方法定义 start =============
 # 开始祈福
 def start(private_key):
+    # 合约地址: https://app.roninchain.com/address/ronin:9d3936dbd9a794ee31ef9f13814233d435bd806c
+    blessing_contract_address='0x9d3936dbd9a794ee31ef9f13814233d435bd806c'
+    ronin_rpc = 'https://api.roninchain.com/rpc'
+    abi_file_path = os.path.abspath('blessing_abi.json')
     provider = Web3.HTTPProvider(ronin_rpc)
     w3 = Web3(provider)
 
@@ -23,7 +24,7 @@ def start(private_key):
     signer = w3.eth.account.from_key(private_key)
     address = signer.address
     if has_currently_activated(contract, address):
-        print(f"Blessing has been activated for {address}")
+        print(f"Blessing had been activated for {address}")
         return
     if activate_streak(w3, contract, signer):
         print(f"Blessing activate success for {address}")
@@ -48,3 +49,16 @@ def activate_streak(w3, contract, signer):
     except Exception as e:
         print(f"Blessing failed for {signer.address} - {e}")
         return False
+### ============方法定义 end =============
+
+### ============脚本执行 start =============
+# 从.blessing_keys文件中读取想要祈福的账号
+with open('.batch_blessing_env', 'r') as file:
+  # 使用 json.load() 方法加载数据
+  keys = json.load(file)
+
+# 遍历所有账号，挨个祈福
+for key in keys:
+  start(key)
+
+### ============脚本执行 end =============
